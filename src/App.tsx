d42b28072cf59561ca9d948b935c0167e0b1dc25
digitrom <roma.bobrovski@gmail.com>
2023-02-14 17:40:55 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
+
+
+type TodosType = {
+    completed: boolean
+    id: number
+    title: string
+    userId: number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // useEffect(() =>{
+    //   fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //       .then(response => response.json())
+    //       .then(json => console.log(json))
+    // },[])
+
+    const [todos, setTodos] = useState<TodosType[]>([])
+    console.log(todos)
+
+    const showUpHandler = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }
+
+    const mappedTodos = todos.map(t => {
+            return (
+                <div className="App">
+                    <li key={t.id}>
+                        <span>{t.id}</span>
+                        <span>{t.title}</span>
+                        <input type="checkbox" checked={t.completed}></input>
+                    </li>
+                </div>
+            )
+        })
+    return (
+        <div className="App">
+            <Button
+                name={"Show up"}
+                callBack={showUpHandler}
+            />
+            <ul>
+                {mappedTodos}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
